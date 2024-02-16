00789d4ac9551777b2bd33614fafbe9e0c3c52e3
sattarov1960 <89763917+sattarov1960@users.noreply.github.com>
2024-02-16 22:39:21 +0700

    update mainLogout

---
+++ layout/components/popUp/withdraw/withdraw.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+function Withdraw(){
+    return (
+        <section>
+            <h1>Вывод средств</h1>
+            <Image src="" alt=""/>
+            <div>
+                <div>
+                    <Image></Image>
+                </div>
+                <div>
+                    <Image></Image>
+                </div>
+                <div>
+                    <Image></Image>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Withdraw
--- layout/screens/trade/trade.tsx
+++ layout/screens/trade/trade.tsx
@@ -4,6 +4,7 @@ import styles from "./styles/trade.module.css"
 import SuccessPopUp from "@/layout/components/popUp/success/success";
 import TradePopUp from "@/layout/components/popUp/trade/trade";
 import ErrorPopUp from "@/layout/components/popUp/error/error";
+import Withdraw from "@/layout/components/popUp/withdraw/withdraw";
 
 export function Trade({id}: {id: string | undefined}) {
     return (
